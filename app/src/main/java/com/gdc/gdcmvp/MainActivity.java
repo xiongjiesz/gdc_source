762acc366ed1bfe867e74e3db6e5a430035ed347
xiongjiesz <413875082@qq.com>
2018-03-18 18:13:25 +0800

    git hub第一次增量更新测试

--- app/src/main/java/com/gdc/gdcmvp/MainActivity.java
+++ app/src/main/java/com/gdc/gdcmvp/MainActivity.java
@@ -4,6 +4,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 
+/**
+ * git hub 第一次增量更新测试
+ */
 public class MainActivity extends AppCompatActivity {
 
     @Override
